4982dbf48e5dec657d8dd8b9d242b448c0d0d3ef
Kuinox <nicolas.vandeginste@signature-code.com>
2020-06-09 11:23:36 +0200

    enabled nullattribute to internal.

--- Tests/CK.Text.Tests/NullAttribute.cs
+++ Tests/CK.Text.Tests/NullAttribute.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 #define INTERNAL_NULLABLE_ATTRIBUTES
-
 namespace System.Diagnostics.CodeAnalysis
 {
     /// <summary>Specifies that the method or property will ensure that the listed field and property members have not-null values.</summary>
